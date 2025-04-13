3e3994dc5d823d64d6840ef4208866d90385939c
Cao Bá Sơn <24100038@st.phenikaa-uni.edu.vn>
2025-04-13 22:55:08 +0700

    Khởi tạo project

---
+++ nhom10_quanlydoanvien/src/main/java/com/baitaplon/main/Nhom10_quanlydoanvien.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.baitaplon.main;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Nhom10_quanlydoanvien {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
